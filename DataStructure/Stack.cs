801a3f5e2d78f2d546fc282fd292349da7e8d8f5
kashif <mkashif.butt@hotmail.com>
2019-04-08 16:30:08 +0400

    Add project files.

---
+++ DataStructure/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    
+    public class Stack<T>
+    {
+        private List<T> _stack = new List<T>();
+        public Stack()
+        {
+
+        }
+
+        public void Push(T item)
+        {
+            if (item != null)
+            {
+                _stack.Add(item);
+            }
+
+        }
+
+        public void Pop()
+        {
+            if (_stack.Count > 0)
+            {
+
+                _stack.RemoveAt(_stack.Count - 1);
+            }
+        }
+
+        public T Peek()
+        {
+            //DRAWback of using Generics, you cannot return default generic as default can be a actual value in the stack.
+            //If peek is called on an empty stack it will throw an error.
+            return _stack[_stack.Count - 1];
+        }
+    }
+}
---
+++ DataStructureImplementation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataStructureImplementation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DataStructure.Stack<string> stack = new DataStructure.Stack<string>();
+        
+            stack.Push("1");
+            Console.WriteLine(stack.Peek());
+            stack.Push("3");
+            Console.WriteLine(stack.Peek());
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+            Console.ReadLine();
+
+        }
+    }
+}
