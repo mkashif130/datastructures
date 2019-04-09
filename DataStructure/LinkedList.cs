5ff767f99afeb5ce118d78e63ebcd76d2423f33e
kashif <mkashif.butt@hotmail.com>
2019-04-09 12:28:53 +0400

    temp

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+    }
+    public class LinkedList<T>
+    {
+        private Node<T> _nodes = new Node<T>();
+        private Node<T> _head = null;
+        private Node<T> _tail = null;
+
+        public Node<T> Push(T value)
+        {
+            Node<T> node = new Node<T> { Value = value };
+            if (_head == null && _tail == null)
+            {
+                _head = node;
+                _tail = node;
+                _nodes = node;
+            }
+            else
+            {
+                Node<T> firstNode = _head;
+                while (firstNode.Next != null)
+                {
+                    firstNode = firstNode.Next;
+
+                }
+                firstNode.Next = node;
+                _tail = node;
+            }
+
+
+            return node;
+        }
+
+        public Node<T> Prepend(T value)
+        {
+            Node<T> node = new Node<T> { Value = value, Next = _head };
+            if (_head == null && _tail == null)
+            {
+
+                _tail = node;
+            }
+            _head = node;
+            _nodes = node;
+            return node;
+        }
+
+        public void InsertAfter(Node<T> previousNode, T value)
+        {
+
+            Node<T> node = new Node<T> { Value = value, Next = previousNode.Next };
+            if (_head ==null && _tail ==null)
+            {
+                _head = node;
+                _tail = node;
+                _nodes = node;
+            }
+            else
+            {
+                previousNode.Next = node;
+                if (node.Next == null)
+                {
+                    _tail = node;
+                }
+            }
+        }
+
+       
+    }
+}
--- DataStructureImplementation/Program.cs
+++ DataStructureImplementation/Program.cs
@@ -7,16 +7,44 @@ namespace DataStructureImplementation
 
         static void Main(string[] args)
         {
+            LinkedListOperation();
+            //StackOperation();
+            Console.ReadLine();
+
+        }
+        public class Student
+        {
+            public int RollNumber { get; set; }
+            public string name { get; set; }
+        }
+        private static void LinkedListOperation()
+        {
+            DataStructure.LinkedList<Student> linkedList = new DataStructure.LinkedList<Student>();
+            linkedList.Push(new Student { name = "kashif", RollNumber = 1 });
+            linkedList.Push(new Student { name = "asif", RollNumber = 2 });
+            linkedList.Push(new Student { name = "mujtaba", RollNumber = 3 });
+            linkedList.Push(new Student { name = "gautam", RollNumber = 4 });
+            linkedList.Prepend(new Student { name = "muayyad", RollNumber = 0 });
+
+            linkedList.Prepend(new Student { name = "ashraf", RollNumber = -1 });
+
+            linkedList.Prepend(new Student { name = "anwar", RollNumber = -2 });
+
+            linkedList.Prepend(new Student { name = "umesh", RollNumber = -3 });
+
+        }
+
+        private static void StackOperation()
+        {
+
             DataStructure.Stack<string> stack = new DataStructure.Stack<string>();
-        
+
             stack.Push("1");
             Console.WriteLine(stack.Peek());
             stack.Push("3");
             Console.WriteLine(stack.Peek());
             stack.Pop();
             Console.WriteLine(stack.Peek());
-            Console.ReadLine();
-
         }
     }
 }
